c07d856a29cd441e3d61ed0d67b100983c149016
syafiqahraddin <22b2046@ubd.edu.bn>
2023-09-19 19:10:03 +0800

    Update 03-backtracking.R

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -4,12 +4,12 @@
 library(tibble)
 library(dplyr)
 
-mode_backtrack <- function(seq) {
+a_backtrack <- function(seq) {
   seq_length <- length(seq)
   if (seq_length < 3) {
     return(FALSE)
   }
-  
+#For-Loop
   for (i in 2 :(seq_length - 1)) {
     if (seq[i] < seq[1] && seq[i+1] > seq[i]) {
       return(TRUE)
@@ -21,10 +21,23 @@ mode_backtrack <- function(seq) {
 
 backtracks_df <- collatz_df %>%
   group_by(start) %>%
-  filter(any(sapply(seq, mode_backtrack))) %>%
+  filter(any(sapply(seq, a_backtrack))) %>%
   ungroup()
 
 head(backtracks_df)
 
 #2 -------------------------------------------
 
+backtracks_df <- collatz_df %>%
+  filter(start != 1 & max_val > start)
+
+backtrack_counts <- backtrack_df %>%
+  mutate(above_start = map_int(seq, function(seq) sum(seq > start))
+         ) %>%
+  count(above_start)
+
+mode_backtrack <- backtrack_counts %>%
+  filter(n == max(n)) %>%
+  pull(above_start)
+
+mode_backtrack
