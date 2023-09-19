24877bff33b84ca60249cef0e1658b72a0f5243b
izzadanan <22b2125@ubd.edu.bn>
2023-09-19 14:59:17 +0800

    Update solution.R

--- solution.R
+++ solution.R
@@ -6,15 +6,14 @@ library(testthat, quietly = TRUE)
 
 # Task 1 ----------------------------------------------------------------------
 gen_collatz <- function(n) {
-  if (n <= 0 || !is.integer(n)) {
+  if (!is.numeric(n) || n <= 0 || !is.integer(n)) {
     stop("Input must be a positive integer.")
   }
   collatz_seq <- c(n)
   while (n != 1) { 
     if (n %% 2 == 0) {
       n <- n / 2
-    } 
-    else {
+    } else {
       n <- 3 * n + 1
     }
     collatz_seq <- c(collatz_seq, n)
@@ -22,21 +21,17 @@ gen_collatz <- function(n) {
   return(collatz_seq)
 }
 
-collatz_df <- tibble(start = 1:10000)
-collatz_df <- collatz_df %>%
-  rowwise() %>%
-  mutate(seq = list(gen_collatz(start)),
-         length = length(seq),
-         parity = ifelse(last(seq) %% 2 == 0, "Even", "Odd"),
-         max_val = max(seq))
-head(collatz_df)
-
-# Task 1 ---------------------------------------------------------
+collatz_df <- tibble(start = 1:10000, seq = list(rep(NA_real_, 1)),
+                     length = NA_real_, parity = NA_character_, 
+                     max_val = NA_real_)
 for(i in 1:10000) {
   collatz_seq <- gen_collatz(i)
-  collatz_df <- add_row(collatz_df, start = i, seq = list(collatz_seq),
-                        length = length(collatz_seq),
-                        parity = ifelse(collatz_seq[length(collatz_seq)] %% 2== 0,
-                                        "Even", "Odd"),
-                        max_val = max(collatz_seq))
+  collatz_df[i, c("seq", "length", "parity", "max_val")] <- list(
+    list(collatz_seq), length(collatz_seq),
+    ifelse(i %% 2 == 0, "Even", "Odd"),
+    max(collatz_seq)
+  )
 }
+head(collatz_df)
+print(collatz_df)
+
