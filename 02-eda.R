22f90ad12fa3f3dcc6a533d2868085dfe8c0293a
Bibi Fatinah <22b9014@ubd.edu.bn>
2023-09-21 11:43:08 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -39,7 +39,14 @@ print(collatz_df)
 # Task 2 ------------------------------------------------------------
 
 #1 ------------------------------------------------------------------
-top10longest <- collatz_df %>%
+collatz_df_fixed <- collatz_df %>%
+  mutate(
+    seq = map(seq, as.numeric),
+    length = as.numeric(length),
+    parity = as.character(parity),
+    max_val = as.numeric(max_val)
+  )
+top10longest <- collatz_df_fixed %>%
   arrange(desc(length)) %>%
   head(10)
 
