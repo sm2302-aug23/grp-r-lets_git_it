ca6689e7320d533a33cad6e18522c925391ce469
aqilahrafidi <22b2149@ubd.edu.bn>
2023-09-21 10:37:26 +0800

    Update process.R

--- process.R
+++ process.R
@@ -1,3 +1,9 @@
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(tibble)
+library(testthat, quietly = TRUE)
+
 gen_collatz <- function(n) {
   if (!is.numeric(n) || n <= 0 || !is.integer(n)) {
     stop("Input must be a positive integer.")
@@ -34,6 +40,7 @@ for(i in 1:10000) {
 head(collatz_df)
 print(collatz_df)
 
+<<<<<<< Updated upstream
 
 # Task 2 
 collatz_df_fixed <- collatz_df %>%
@@ -43,3 +50,42 @@ collatz_df_fixed <- collatz_df %>%
     parity = as.character(parity),
     max_val = as.numeric(max_val)
   )
+=======
+# task 2 ----------------------
+gen_collatz <- function(n) {
+  if (n <= 0 || !is.integer(n)) {
+    stop("Input must be a positive integer.")
+  }
+  collatz_seq <- c(n)
+  while (n != 1) {
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+    collatz_seq <- c(collatz_seq, n)
+  }
+  return(collatz_seq)
+}
+
+collatz_df <- tibble(start = integer(), seq = list(), length = numeric(), 
+                     parity = character(), max_val = numeric())
+
+for (i in 1:10000) {
+  collatz_seq <- gen_collatz(i)
+  collatz_df <- add_row(collatz_df, start = i, seq = list(collatz_seq),
+                        length = length(collatz_seq), 
+                        parity = ifelse(i %% 2 == 0, "Even", "Odd"), 
+                        max_val = max(collatz_seq))
+}
+head(collatz_df)
+print(collatz_df)
+
+max_val_int <- collatz_df %>%
+  filter(max_val == max(max_val)) %>%
+  select(start, max_val)
+head(max_val_int)
+
+
+
+>>>>>>> Stashed changes
