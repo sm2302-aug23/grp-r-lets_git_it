4f8f90aba0498139b427da3e68370906532665a4
Bibi Fatinah <22b9014@ubd.edu.bn>
2023-09-21 15:00:12 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -40,7 +40,7 @@ print(collatz_df)
 
 #1 ------------------------------------------------------------------
 
-top10longest <- collatz_df_fixed %>%
+top10longest <- collatz_df %>%
   arrange(desc(length)) %>%
   head(10) %>%
   select(start)
