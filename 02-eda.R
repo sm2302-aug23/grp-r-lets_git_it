d887b4a3b3aa6d16dc490e4df8ab884d05d98b58
aqilahrafidi <22b2149@ubd.edu.bn>
2023-09-21 09:56:42 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -1,15 +1,17 @@
-library(tidyverse)
-library(tidyr)
-library(dplyr)
-library(tibble)
-library(testthat, quietly = TRUE)
+# Load libraries ----------------------------------------------------------
+ library(tidyverse)
+ library(tidyr)
+ library(dplyr)
+ library(tibble)
+ library(testthat, quietly = TRUE)
 
+# Task 1 ------------------------------------------------------------------
 gen_collatz <- function(n) {
-  if (!is.numeric(n) || n <= 0 || !is.integer(n)) {
+  if (n <= 0 || !is.integer(n)) {
     stop("Input must be a positive integer.")
   }
   collatz_seq <- c(n)
-  while (n != 1) { 
+  while (n != 1) {
     if (n %% 2 == 0) {
       n <- n / 2
     } else {
@@ -20,21 +22,21 @@ gen_collatz <- function(n) {
   return(collatz_seq)
 }
 
-collatz_df <- tibble(start = 1:10000, seq = list(rep(NA_real_, 1)),
-                     length = NA_real_, parity = NA_character_, 
-                     max_val = NA_real_)
-for(i in 1:10000) {
+collatz_df <- tibble(start = integer(), seq = list(), length = numeric(), 
+                     parity = character(), max_val = numeric())
+
+for (i in 1:10000) {
   collatz_seq <- gen_collatz(i)
-  collatz_df[i, c("seq", "length", "parity", "max_val")] <- list(
-    list(collatz_seq), length(collatz_seq),
-    ifelse(i %% 2 == 0, "Even", "Odd"),
-    max(collatz_seq)
-  )
+  collatz_df <- add_row(collatz_df, start = i, seq = list(collatz_seq),
+                        length = length(collatz_seq), 
+                        parity = ifelse(i %% 2 == 0, "Even", "Odd"), 
+                        max_val = max(collatz_seq))
 }
 head(collatz_df)
 print(collatz_df)
 
-# Task 2------------------------------------------------------------
+
+# Task 2 ------------------------------------------------------------
 
 #1 ------------------------------------------------------------------
 
@@ -71,6 +73,3 @@ summary_stats_odd <- collatz_df %>%
     sd_length_odd = sd(length)
   )
 head(summary_stats_odd)
-
-
-
