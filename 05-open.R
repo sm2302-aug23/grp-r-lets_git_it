4215beb733e264bdb5aa4fca8c4e9fc10769547c
aqilahrafidi <22b2149@ubd.edu.bn>
2023-09-23 21:29:41 +0800

    loaded some libraries to 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -1,1 +1,8 @@
-# Task 5 -----------------------------------------------------
+# Load libraries -------------------------------------------------------------
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(tibble)
+library(testthat, quietly = TRUE)
+
+# Task 5 ---------------------------------------------------------------------
