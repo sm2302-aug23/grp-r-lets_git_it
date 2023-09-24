529fa37992cc2b23c46b00d7e382678931ae1963
aqilahrafidi <22b2149@ubd.edu.bn>
2023-09-24 12:48:43 +0800

    added findings to 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -6,3 +6,17 @@ library(tibble)
 library(testthat, quietly = TRUE)
 
 # Task 5 ---------------------------------------------------------------------
+
+# Analysing the arithmetic progressions in 
+# stopping times of the Collatz Conjecture 
+
+# THE HYPOTHESIS
+
+# The starting integer can affect 
+# the number of steps it takes a sequence to reach one
+
+# 1) Odd number tend to produce a longer sequence (Especially prime numbers)
+# 2) Even number tend to produce shorter sequence 
+
+
+
